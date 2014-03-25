fe536739a4b37a830cb8fa89016b0df8ca0f7898
Taylor Martin <taylormartin357@gmail.com>
2014-03-25 17:26:06 -0400

    Updated for parking passes

--- main.cpp
+++ main.cpp
@@ -32,10 +32,13 @@ void signIn();
 void signOut();
 void listIn();
 bool isIn(string name);
+void parking();
+bool isParking(string name);
 
 //string getAlias(string name);
 bool startHere();
 vector<string> inList;
+vector<string> parkingList;
 vector<time_t> inTime;
 
 
@@ -47,7 +50,7 @@ int main () {
 }
 
 bool startHere() {
-	dispHelp();
+	//dispHelp();
 	cout << endl;
 	string mode;
 	cout << "\033[;32mReady: \033[0m";
@@ -58,7 +61,8 @@ bool startHere() {
 			case 'i': signIn(); break;
 			case 'o': signOut(); break;
 			case 'l': listIn(); break;
-			//case 'e': signAllOut(); return true; break;
+			case 'p': parking(); break;
+			default: dispHelp(); break;
 		}
 	} catch (const out_of_range& oor) {
 		cout << "\033[;31mError\033[0m" << endl;
@@ -69,9 +73,9 @@ bool startHere() {
 void dispHelp() {
 	cout << "\033[0;33mSign In: i name" << endl;
 	cout << "Sign Out: o name work_peformed" << endl;
-	cout << "List Users In: l\033[0m" << endl;
-	//cout << "Alias: a record_name alias_name" << endl;
-	//cout << "Exit: e password" << endl;
+	cout << "List Users In: l" << endl;
+	cout << "Parking Pass: p i/o last_digit name" << endl;
+	cout << "\033[0m";
 }
 
 void signIn() {
@@ -130,6 +134,10 @@ void signOut() {
 		cout << "\033[;31m" << name << " is already signed out" << "\033[0m" << endl;
 		return;
 	}
+	if (isParking(name)) {
+		cout << "\033[;31m" << "You must return the pass first!" << "\033[0m" << endl;
+		return;
+	}
 
 	int listPos = 0;
 	for(int i=0; i < inList.size(); i++)
@@ -164,30 +172,13 @@ void signOut() {
 	myfile.close();
 
 	cout << "\033[;36m" << name << " has logged out at " << buffer << " spending " << minIn << " minutes.\033[0m " << work << endl;
-
-
 }
-
-/*void signAllOut() {
-	string name;
-	cin >> name;
-	const int length = name.length();
-	for(int i=0; i < length; i++)
-	{
-		name[i] = tolower(name[i]);
-	}
-}*/
-
-/*string getAlias(string name) {
-	return name;
-}*/
 
 bool isIn(string name) {
 	for(int i=0; i < inList.size(); i++)
 		if (inList[i].compare(name) == 0)
 			return true;
 	return false;
-
 }
 
 void listIn() {
@@ -199,4 +190,51 @@ void listIn() {
 	cout << "\033[0m" << endl;
 }
 
+void parking() {
+	string action, number, name;
+	cin >> action;
+	cin >> number;
+	cin >> name;
 
+	const int length = name.length();
+	for(int i=0; i < length; i++)
+	{
+		name[i] = tolower(name[i]);
+	}
+	//name = getAlias(name);
+	if (!isIn(name)) {
+		cout << "\033[;31m"<< name << ", you must be signed in to use parking passes!\033[0m" << endl;
+		return;
+	}
+
+	time_t rawtime;
+	struct tm * timeinfo;
+	char buffer [80];
+
+	time (&rawtime);
+	timeinfo = localtime (&rawtime);
+
+	strftime (buffer,80,"%Y-%m-%d@%R",timeinfo);
+
+	if (tolower(action[0]) == 'o') {
+		cout << "\033[;36m" << name << " took a parking pass on " << buffer << ".\033[0m" << endl;
+		parkingList.push_back(name);
+	} else if (tolower(action[0]) == 'i') {
+		cout << "\033[;36m" << name << " returned a parking pass on " << buffer << ".\033[0m" << endl;
+		int listPos = 0;
+		for(int i=0; i < parkingList.size(); i++)
+			if (parkingList[i].compare(name) == 0) {
+				parkingList.erase(parkingList.begin()+i);
+				listPos = i;
+			}
+	} else {
+		dispHelp();
+	}
+}
+
+bool isParking(string name) {
+	for(int i=0; i < parkingList.size(); i++)
+		if (parkingList[i].compare(name) == 0)
+			return true;
+	return false;
+}
